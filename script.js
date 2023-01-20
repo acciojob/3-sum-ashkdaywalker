ebb4b7281a2de118cd2f95555e10125f417d90dd
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-01-20 14:21:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function threeSum(arr, target) {
+function threeSum(S, target) {
+  S.sort((a,b) => a-b);
+  let closetSum = Infinity;
+  for(let i = 0;i< S.length-2;i++){
+    let left = i+1,right = S.length -1;
+    while(left < right){
+      let currentSum = S[i] + S[left] + S[right];
+      if(Math.abs(currentSum - target)< Math.abs(closetSum - target)){
+        closetSum = currentSum;
+      }
+      if(currentSum < target){
+        right--;
+      } else{
+        return currentSum;
+      }
+    }
+  }
+  return closetSum;
   //your code here
 }
 
